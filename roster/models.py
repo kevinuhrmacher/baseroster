8db1defc38a24339128f14d0b782132a1eb64e92
kevinuhrmacher <kevin.uhrmacher@gmail.com>
2014-02-28 23:01:29 -0500

    database set, models need a little tweaking

--- roster/models.py
+++ roster/models.py
@@ -4,35 +4,48 @@ from django.db import models
 
 class Player(models.Model):
     number = models.IntegerField(unique=True, null=True, max_length=2, verbose_name="No.")
-    lastname = models.CharField(max_length=50)
-    firstname = models.CharField(max_length=50)
-    
+    name = models.CharField(max_length=70)
     class Meta(object):
         verbose_name_plural = "Players"
-        ordering = ('number',)
+        ordering = ('name', 'number')
     
     def __unicode__(self):
-        return self.number
+        return self.name
     
     def save(self, *args, **kwargs):
-        self.lastname = self.lastname.upper()
         super(Player, self).save(*args, **kwargs)
-
-    hand = models.CharField(null=True, max_length=5)
-    position = models.TextField(null=True)
+        
+    imgurl = models.TextField(null=True)
+    dominant_hand = models.CharField(null=True, max_length=3)
+    position = models.CharField(null=True, max_length=6)
     height = models.CharField(null=True, max_length=3, verbose_name="Height")
     weight = models.IntegerField(null=True, max_length=3, verbose_name="Weight")
     year = models.CharField(null=True, max_length=30)
-    hometown = models.CharField(null=True, max_length=140, verbose_name="Hometown")
-    highschool = models.CharField(null=True, max_length=140, verbose_name="High School")
+    hometown = models.TextField(null=True, verbose_name="Hometown")
+    high_school = models.CharField(null=True, max_length=140, verbose_name="High School")
+    batting_avg = models.IntegerField(null=True)
+    gp_gs = models.IntegerField(null=True)
+    at_bats = models.IntegerField(null=True)
+    runs = models.IntegerField(null=True)
+    hits = models.IntegerField(null=True)
+    doubles = models.IntegerField(null=True)
+    homeruns = models.IntegerField(null=True)
+    rbi = models.IntegerField(null=True)
+    total_bases = models.IntegerField(null=True)
+    slugging = models.IntegerField(null=True)
+    walks = models.IntegerField(null=True)
+    strikeouts = models.IntegerField(null=True)
+    on_base_percentage = models.IntegerField(null=True)
+    assists = models.IntegerField(null=True)
+    errors = models.IntegerField(null=True)
     story = models.TextField(null=True)
     
     
     class Meta(object):
-        ordering = ('number', 'position')
+        ordering = ('name', 'position')
         
     def __unicode__(self):
-        return U'%s' %(self.number)
+        return U'%s' %(self.name)
     
     
     
@@ -47,7 +60,7 @@ class Coach(models.Model):
         return self.name
     
     def save(self, *args, **kwargs):
-        self.name = self.name.upper()
+        self.name = self.name()
         super(Player, self).save(*args, **kwargs)
 
     coachingage = models.CharField(null=True, max_length=2)
